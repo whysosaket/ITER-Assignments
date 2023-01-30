161f10a29ca1a846adc765f5f55ce5eafd43acca
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-01-31 03:36:20 +0530

    Update Question3.java

--- Assignment-5 (Java)/Question3.java
+++ Assignment-5 (Java)/Question3.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        //  Write a program to swap the ith bit with jth bit of a 64-bits integer number.
+        // Take input of the 64-bit integer number
         long num = sc.nextLong();
+        // Take input of the position of the first bit
         int i = sc.nextInt();
+        // Take input of the position of the second bit
         int j = sc.nextInt();
+        // Create a mask for the first bit
         long mask = 1;
+        // Create a mask for the second bit
         long mask1 = 1;
+        // Shift the first mask to the i-th position
         mask = mask << i;
+        // Shift the second mask to the j-th position
         mask1 = mask1 << j;
+        // Extract the value of the i-th bit
         long bit1 = num & mask;
+        // Extract the value of the j-th bit
         long bit2 = num & mask1;
+        // Swap the i-th bit with the j-th bit
         if (bit1 == 0) {
             num = num & ~mask1;
         } else {
@@ -23,7 +32,7 @@ public class Question3 {
         } else {
             num = num | mask;
         }
+        // Output the new value of the 64-bit integer number
         System.out.println(num);
-        
     }
 }
