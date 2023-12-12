91ba4f020a3603b70ac075b1286a8813be5a39cd
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-12-13 02:54:51 +0530

    Assignment 3 Q8 FIX

--- SEM-5/PIP/Assignment-3/Question8.py
+++ SEM-5/PIP/Assignment-3/Question8.py
@@ -4,7 +4,7 @@ def is_perfect_natural(a):
     if a <= 0:
         return False
     sum = 0
-    for i in range(1,int(math.sqrt(a)+2)):
+    for i in range(1,a//2+2):
         if a%i==0:
             sum+=i
     return sum==a
