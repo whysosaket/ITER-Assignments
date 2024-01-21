fcb8be651b6188a75694ffb689b5a9035ccd6192
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-01-22 04:03:36 +0530

    Update q14.py

--- SEM-5/PIP/Assignment-8/q14.py
+++ SEM-5/PIP/Assignment-8/q14.py
@@ -1,10 +1,8 @@
-def deleteAtKthPos(lst, k, i=0, n=1):
-    if i == len(lst):
+def deleteAtKthPos(lst, k, i=0):
+    if i+k-1 >= len(lst):
         return lst
-    if n == k:
-        lst.pop(i)
-        n = 1
-    return deleteAtKthPos(lst, k, i+1, n+1)
+    lst.pop(i+k-1)
+    return deleteAtKthPos(lst, k, i+k-1)
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 k = 3
