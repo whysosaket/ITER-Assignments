3111ad24a47e8ee3a42b4da393f8a4fd892db60b
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-01-22 03:58:10 +0530

    Fixes

--- SEM-5/PIP/Assignment-2/q7.py
+++ SEM-5/PIP/Assignment-2/q7.py
@@ -1,7 +1,9 @@
 import math
 
 def area_triangle(side1, side2, side3):
-    if side1+side2<=side3 or side2+side1<=side3 or side3+side2<=side1:
+    try:
+        assert side1+side2<=side3 or side2+side1<=side3 or side3+side2<=side1
+    except:
         return "Not a valid triangle"
     s = (side1+side2+side3)/2
     area = math.sqrt(s*(s-side1)*(s-side2)*(s-side3))
--- SEM-5/PIP/Assignment-8/q13.py
+++ SEM-5/PIP/Assignment-8/q13.py
@@ -1,9 +1,8 @@
 def insertAtKthPos(lst, x, k, i=0):
-    if i == len(lst):
+    if i+k >= len(lst):
         return lst
-    if i%k == 0 and i != 0:
-        lst.insert(i, x)
-    return insertAtKthPos(lst, x, k, i+1)
+    lst.insert(i+k, x)
+    return insertAtKthPos(lst, x, k, i+k+1)
 
 lst = [1, 2, 3, 4, 5, 6, 7]
 x = 50
